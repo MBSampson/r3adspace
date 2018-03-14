a0ce8ceaa5d38d114eeb110033900b2e5296ab24
Matt Sampson <matthew@therubythree.com>
2018-03-14 16:53:56 -0700

    Renamed submit_form_helper to form_button_helper and added additional functionality to the helper so that it can be used on the Ad show page.

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -10,6 +10,7 @@ class AdsController < ApplicationController
   # GET /ads/1
   # GET /ads/1.json
   def show
+    @form_type = 'show'
   end
 
   # GET /ads/new
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,18 @@
 module ApplicationHelper
-  def submit_form_helper(form_type)
+  def form_button_helper(form_type)
     if form_type == "edit"
-      '<input class="btn btn-warning" data-disable-with="Update Ad" name="commit" type="submit" value="Update Ad">'.html_safe
+
+      ('<input class="btn btn-info" data-disable-with="Update Ad" name="commit" type="submit" value="Update Ad"> | '.html_safe) +
+      (link_to "Destroy", @ad, method: :delete, data: { confirm: "Are you sure?" }, class: "btn btn-danger") +
+      (' | ').html_safe +
+      (link_to "Cancel", @ad, class: "btn btn-light")
     elsif form_type == 'new'
-      '<input class="btn btn-success" data-disable-with="Create Ad" name="commit" type="submit" value="Create Ad">'.html_safe
+      '<input class="btn btn-success" data-disable-with="Create Ad" name="commit" type="submit" value="Create Ad"> | '.html_safe +
+      (link_to "Cancel", '/', class: "btn btn-light")
+    elsif form_type == 'show'
+      (link_to "Edit", edit_ad_path(@ad), class: "btn btn-info") +
+      (' | ').html_safe +
+      (link_to "Back", ads_path, class: "btn btn-primary")
     else
       content_tag(:p, "Not a valid form_type...")
     end
