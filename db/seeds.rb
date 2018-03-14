41c32611c1a81dac1b1ec9f7679a8f7817df60df
Matt Sampson <matthew@therubythree.com>
2018-03-13 17:13:38 -0700

    Created scaffold for Ads and set homepage to Ads#index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ad.create!(
+  title: "Ad 1",
+  description: "Archon subtype augmented subtype automatic hit bard comatose concentrate on a spell coup de grace direct a spell disease effective hit point increase falling fraction grab granted power masterwork monstrous humanoid type negative energy plane orison regeneration skill rank special ability strength tiny total cover trade domain treasure undead type untrained. Base attack bonus charm creature enchantment extraordinary ability fey type fortitude save healing subschool immunity inherent bonus melee touch attack overlap profane bonus protection domain scalykind domain scribe skill sorcerer spell slot trickery domain.",
+  image: "http://via.placeholder.com/150x150"
+)
