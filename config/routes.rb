f5cf34be8195c250bb622ac9b9783b26ead052e9
Matt Sampson <matthew@therubythree.com>
2018-03-16 14:57:09 -0700

    User NEW and UPDATE paths now allow for first_name, last_name, and image params

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '', path_names: {
-    sign_in:    'login',
-    sign_out:   'logout',
-    sign_up:    'register'
-  }
+  devise_for :users, :controllers => {:registrations => "users/registrations"}
+  
   resources :ads
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'filter', to: 'ads#filter', as: 'ads_filter'
