24517e36ca998b460b7222fd4910e36f7391318e
Matt Sampson <matthew@therubythree.com>
2018-03-16 14:15:27 -0700

    Styled sign up and began work on edit profile page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,7 +20,8 @@ module ApplicationHelper
 
   def login_container_helper
     if current_user
-      link_to "Logout", destroy_user_session_path, method: :delete, class: 'nav-link'
+      (link_to "Profile Settings", edit_user_registration_path, class: 'nav-link') +
+      (link_to "Logout", destroy_user_session_path, method: :delete, class: 'nav-link')
     else
       (link_to "Sign Up", new_user_registration_path, class: 'nav-link') +
       (link_to "Login", new_user_session_path, class: 'nav-link')
