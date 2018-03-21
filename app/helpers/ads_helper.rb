cdfd4da0ec7b69dff816d2e138eacc1977741377
Matt Sampson <matthew@therubythree.com>
2018-03-21 16:56:30 -0700

    Created three ad helper functions to DRY up code (display_edit_btns, display_published_toggle_btns, show_ad_published_status)

--- app/helpers/ads_helper.rb
+++ app/helpers/ads_helper.rb
@@ -1,9 +1,30 @@
 module AdsHelper
-  def showAdPublishedStatus(page, ad)
+  def show_ad_published_status(page, ad)
     if page == "poster_filter"
       content_tag :div, :class => "ad-published-status" do
         content_tag :span, ad.published
       end
     end
   end
+
+  def display_published_toggle_btns(ad)
+    if user_signed_in?
+      if current_user.id == ad.user_id
+        if ad.published == "draft"
+          link_to "Post", toggle_published_ad_path(ad.id), class: "btn btn-success"
+        else
+          link_to "Hide", toggle_published_ad_path(ad.id), class: "btn btn-danger"
+        end
+      end
+    end
+  end
+
+  def display_edit_btns(ad)
+    if user_signed_in?
+      if current_user.id == ad.user_id
+        form_button_helper(@form_type)
+      end
+    end
+  end
+
 end
