49f986669ef0dd603cb85a2204104d8dc461d3d5
Matt Sampson <matthew@therubythree.com>
2018-03-22 11:25:05 -0700

    Added validations to Ad model

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -4,7 +4,9 @@ class Ad < ApplicationRecord
   after_initialize :init
 
   validates_presence_of :title, :description, :image, :price, :category_id
-
+  validates :price,
+            numericality: true,
+            :numericality => { :greater_than_or_equal_to => 0 }
   def init
     self.published ||= "draft"
   end
